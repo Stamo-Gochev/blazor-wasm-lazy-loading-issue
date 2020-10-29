82bf8ce59cbcf848eab59b656b55c33aaa01702b
Stamo-Gochev <stamo_gochev@yahoo.com>
2020-10-29 13:56:19 +0200

    feat: update dependency component

---
+++ BlazorWasmLazyLoading/MyComponentsDependency1/DependencyComponent.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyComponentsDependency1
+{
+    public partial class DependencyComponent : ComponentBase
+    {
+        public List<int> Data { get; set; } = new List<int>() { 11, 12, 13 };
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            var data = new List<int>();
+            data.Add(1);
+            data.Add(2);
+            data.Add(100);
+
+            var queryable = data.AsQueryable();
+
+            var result = queryable.Where(x => x > 1);
+
+            Data.AddRange(result.ToList());
+
+            base.OnAfterRender(firstRender);
+        }
+
+    }
+}
