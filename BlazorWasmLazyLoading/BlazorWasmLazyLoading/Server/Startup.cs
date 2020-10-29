116ad0e5832de155be6ac09a2d88cdc9a0035da5
Stamo-Gochev <stamo_gochev@yahoo.com>
2020-10-29 15:59:33 +0200

    feat: add sample

--- BlazorWasmLazyLoading/BlazorWasmLazyLoading/Server/Startup.cs
+++ BlazorWasmLazyLoading/BlazorWasmLazyLoading/Server/Startup.cs
@@ -23,15 +23,6 @@ namespace BlazorWasmLazyLoading.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-            //services.AddControllersWithViews();
-            //services.AddRazorPages();
-
-            //services.AddScoped<LazyAssemblyLoader>();
-
-
-            //services.AddControllersWithViews();
-            //services.AddServerSideBlazor();
             services.AddRazorPages();
             services.AddScoped<LazyAssemblyLoader>();
         }
